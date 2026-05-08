dec9753df5c9a35ab710ebe60368d7e6b594ee25
zafranga88 <zfrancogabriel@hotmail.com>
2026-05-07 23:12:34 -0300

    midterm

---
+++ nodemon.json
@@ -0,0 +1,8 @@
+{
+    "ext": "js css ejs env",
+    "exec": "node --env-file=.env server.js",
+    "ignore": [
+        ".git",
+        "node_modules"
+    ]
+}
---
+++ server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+// Define the the application environment
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Define the port number the server will listen on
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+/**
+  * Configure Express middleware
+  */
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+  * Routes
+  */
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/home.html'));
+});
+
+app.get('/organizations', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/organizations.html'));
+});
+
+app.get('/projects', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/projects.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://127.0.0.1:${PORT}`);
+  console.log(`Environment: ${NODE_ENV}`);
+});

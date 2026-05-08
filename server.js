78410f886d9bb56a771a0d28b143ed0ba4c636ce
zafranga88 <zfrancogabriel@hotmail.com>
2026-05-08 01:50:06 -0300

    Initial Site Creation

--- server.js
+++ server.js
@@ -44,6 +44,11 @@ app.get('/projects', async (req, res) => {
     res.render('projects', { title });
 });
 
+app.get('/categories', async (req, res) => {
+    const title = 'Categories';
+    res.render('categories', { title });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://127.0.0.1:${PORT}`);
   console.log(`Environment: ${NODE_ENV}`);

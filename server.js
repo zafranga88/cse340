c0e5be10276bc6f82175c55dd0452e0594b34df8
zafranga88 <zfrancogabriel@hotmail.com>
2026-05-20 02:24:15 -0300

    w03

--- server.js
+++ server.js
@@ -30,6 +30,20 @@ app.set('view engine', 'ejs');
 // Tell Express where to find your templates
 app.set('views', path.join(__dirname, 'src/views'));
 
+// Middleware to log all incoming requests
+app.use((req, res, next) => {
+    if (NODE_ENV === 'development') {
+        console.log(`${req.method} ${req.url}`);
+    }
+    next(); // Pass control to the next middleware or route
+});
+
+// Middleware to make NODE_ENV available to all templates
+app.use((req, res, next) => {
+    res.locals.NODE_ENV = NODE_ENV;
+    next();
+});
+
 /**
  * Routes
  */
